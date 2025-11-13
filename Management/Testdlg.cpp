0d2940260df7e497fa19a5d614a9d00ab3fdc6d2
MEIYBAO <15867360+meiybao@user.noreply.gitee.com>
2025-11-13 19:15:18 +0800

    static登录

---
+++ Management/Testdlg.cpp
@@ -0,0 +1,34 @@
+﻿// Testdlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Management.h"
+#include "afxdialogex.h"
+#include "Testdlg.h"
+
+
+// Testdlg 对话框
+
+IMPLEMENT_DYNAMIC(Testdlg, CDialogEx)
+
+Testdlg::Testdlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+Testdlg::~Testdlg()
+{
+}
+
+void Testdlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(Testdlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Testdlg 消息处理程序
