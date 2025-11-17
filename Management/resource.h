fac35903567e3a2216c5e7f81e163e800763f09c
MEIYBAO <15867360+meiybao@user.noreply.gitee.com>
2025-11-17 15:09:56 +0800

    add picture

--- Management/resource.h
+++ Management/resource.h
@@ -8,6 +8,7 @@
 #define IDD_MANAGEMENT_DIALOG           102
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
+#define IDD_DIALOG2                     132
 #define login_button                    1000
 #define exit_button                     1001
 #define username_input                  1002
@@ -17,7 +18,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
