9a9829240fb322d719813553e8d4969115301268
MEIYBAO <15867360+meiybao@user.noreply.gitee.com>
2025-12-08 14:12:01 +0800

    1

--- Management/managerdlg.h
+++ Management/managerdlg.h
@@ -19,25 +19,31 @@ public:
 #endif
 
 protected:
-	CListCtrl m_list;
-	CDatabaseHelper m_dbHelper;
+	CListCtrl m_list;  //列表
+	CDatabaseHelper m_dbHelper;  //数据库类
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnBnClickedstudent_show();
 	afx_msg void OnBnClickedteacher_show();
-	virtual BOOL OnInitDialog();
 	afx_msg void OnBnClickedcourse_show();
 	afx_msg void OnBnClickedcounselor_show();
 
 	afx_msg void OnListRClick(NMHDR* pNMHDR, LRESULT* pResult);
 	afx_msg void OnMenuAdd();
 	afx_msg void OnMenuDelete();
+	afx_msg void OnDblclkList(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnEditKillFocus();
+	afx_msg void OnEditKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void manager::OnEnChangeSemesterEdit();
+	afx_msg void OnBnClickedchangeid();
 
 	virtual BOOL PreTranslateMessage(MSG* pMsg); 
 
 private:
+	virtual BOOL OnInitDialog();
+
 	CString m_currentTable = _T("无");
 	CEdit editItem;
 	CEdit m_semesterEdit;
@@ -46,10 +52,4 @@ private:
 
 	int hitRow;
 	int hitCol;
-public:
-	afx_msg void OnDblclkList(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnEditKillFocus();
-	afx_msg void OnEditKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
-	afx_msg void manager::OnEnChangeSemesterEdit();
-	afx_msg void OnBnClickedchangeid();
 };
